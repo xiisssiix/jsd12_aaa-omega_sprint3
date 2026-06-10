9c9b808c0efaeddd5392e88bdfe585495a0cafb2
Manit Thongkaw <manitthongkaw@gmail.com>
2026-06-10 10:07:18 +0700

    fix: rateLimit.js

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -4,8 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 export const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: isProd ? process.env.RATELIMIT : 1000,
-	standardHeaders: "draft-8",
-	legacyHeaders: false,
-	ipv6Subnet: 56
+	max: isProd ? process.env.RATELIMIT : 1000,
+	standardHeaders: true,
+	legacyHeaders: false
 });
