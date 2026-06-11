3a3e9f528fe160b1c230927272eccf7695c642ed
unknown <xiisssiix@gmail.com>
2026-06-11 15:09:31 +0700

    JWT

--- src/services/jwt.js
+++ src/services/jwt.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export const signAccessToken = (user) => {
   return jwt.sign({
     id: user._id,
-    // role: user.role
+    userNumber: user.userNumber
   },
   process.env.JWT_SECRET,
   {
